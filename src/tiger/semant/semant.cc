6265ccb58e946b64c411a48a165a3e46f0d051ff
WangTianxia <sometimesnaive@sjtu.edu.cn>
2019-11-05 06:26:25 +0000

    [lab4] Stage changes

--- src/tiger/semant/semant.cc
+++ src/tiger/semant/semant.cc
@@ -37,74 +37,213 @@ namespace A {
 
 TY::Ty *SimpleVar::SemAnalyze(VEnvType venv, TEnvType tenv,
                               int labelcount) const {
-  // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  E::EnvEntry *envEntry = venv->Look(sym);
+  if (!envEntry) {
+    errormsg.Error(pos, "undefined variable %s", sym->Name().c_str());
+    return TY::VoidTy::Instance();
+  }
+  if (envEntry->kind == E::EnvEntry::FUN) {
+    errormsg.Error(pos, "function variable %s is not a simple value", sym->Name().c_str());
+    return TY::VoidTy::Instance();
+  }
+  return static_cast<E::VarEntry*>(envEntry)->ty;
 }
 
 TY::Ty *FieldVar::SemAnalyze(VEnvType venv, TEnvType tenv,
                              int labelcount) const {
-  // TODO: Put your codes here (lab4).
+  TY::Ty *varType = var->SemAnalyze(venv, tenv, labelcount);
+  if (varType->ActualTy()->kind != TY::Ty::Kind::RECORD) {
+    errormsg.Error(pos, "not a record variable");
+    return TY::VoidTy::Instance();
+  }
+  TY::RecordTy *recordTy = static_cast<TY::RecordTy *>(varType);
+  TY::FieldList *fieldList = recordTy->fields;
+  while (fieldList) {
+    if (fieldList->head->name->Name() == sym->Name()) {
+      return fieldList->head->ty;
+    }
+    fieldList = fieldList->tail;
+  }
+  errormsg.Error(pos, "unknown field %s", sym->Name().c_str());
   return TY::VoidTy::Instance();
 }
 
 TY::Ty *SubscriptVar::SemAnalyze(VEnvType venv, TEnvType tenv,
                                  int labelcount) const {
-  // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  TY::Ty *varType = var->SemAnalyze(venv, tenv, labelcount);
+  if (varType->ActualTy()->kind != TY::Ty::Kind::ARRAY) {
+    errormsg.Error(pos, "not an array variable");
+    return TY::VoidTy::Instance();
+  }
+  TY::Ty *subscriptType = subscript->SemAnalyze(venv, tenv, labelcount);
+  if (!subscriptType->IsSameType(TY::IntTy::Instance())) {
+    errormsg.Error(pos, "integer required");
+    return TY::VoidTy::Instance();
+  }
+  return varType->ActualTy();
 }
 
 TY::Ty *VarExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
-  // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  TY::Ty *varType = var->SemAnalyze(venv, tenv, labelcount);
+  return varType->ActualTy();
 }
 
 TY::Ty *NilExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
-  // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  return TY::NilTy::Instance();
 }
 
 TY::Ty *IntExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
-  // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  return TY::IntTy::Instance();
 }
 
 TY::Ty *StringExp::SemAnalyze(VEnvType venv, TEnvType tenv,
                               int labelcount) const {
-  // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  return TY::StringTy::Instance();
 }
 
 TY::Ty *CallExp::SemAnalyze(VEnvType venv, TEnvType tenv,
                             int labelcount) const {
   // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  E::EnvEntry *envEntry = venv->Look(func);
+  if (!envEntry) {
+    errormsg.Error(pos, "undefined function %s", func->Name().c_str());
+    return TY::VoidTy::Instance();
+  }
+  if (envEntry->kind == E::EnvEntry::VAR) {
+    errormsg.Error(pos, "%s is not a function", func->Name().c_str());
+    return TY::VoidTy::Instance();
+  }
+  E::FunEntry *funEntry = static_cast<E::FunEntry*>(envEntry);
+  TY::TyList *formals = funEntry->formals;
+  A::ExpList *argList = args;
+  while (formals && argList) {
+    TY::Ty *type_head = formals->head;
+    A::Exp *arg_head = argList->head;
+    TY::Ty *arg_type = arg_head->SemAnalyze(venv, tenv, labelcount);
+    if (!type_head->IsSameType(arg_type)) {
+      errormsg.Error(pos, "parameter type mismatch");
+    }
+    formals = formals->tail;
+    argList = argList->tail;
+  }
+  if (formals) {
+    errormsg.Error(pos, "missing parameters in function %s", func->Name().c_str());
+  }
+  if (argList) {
+    errormsg.Error(pos, "too many parameters in function %s", func->Name().c_str());
+  }
+  TY::Ty *result = funEntry->result;
+  if (!result)
+    return result->ActualTy();
+  else
+    return TY::VoidTy::Instance();
 }
 
 TY::Ty *OpExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
   // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  if (oper == A::PLUS_OP || oper == A::MINUS_OP || oper == A::TIMES_OP || oper == A::DIVIDE_OP) {
+    TY::Ty *left_type = left->SemAnalyze(venv, tenv, labelcount);
+    TY::Ty *right_type = right->SemAnalyze(venv, tenv, labelcount);
+    if (!left_type->IsSameType(TY::IntTy::Instance()) || !right_type->IsSameType(TY::IntTy::Instance())) {
+      errormsg.Error(pos, "integer is required");
+      return TY::IntTy::Instance();
+    }
+  }
+  if (oper == A::EQ_OP || oper == A::NEQ_OP || oper == LT_OP || oper == LE_OP || oper == GT_OP || oper == GE_OP) {
+    TY::Ty *left_type = left->SemAnalyze(venv, tenv, labelcount);
+    TY::Ty *right_type = right->SemAnalyze(venv, tenv, labelcount);
+    if (!left_type->IsSameType(right_type)) {
+      errormsg.Error(pos, "same type required");
+      return TY::IntTy::Instance();
+    }
+  }
+  return TY::IntTy::Instance();
 }
 
 TY::Ty *RecordExp::SemAnalyze(VEnvType venv, TEnvType tenv,
                               int labelcount) const {
   // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  TY::Ty *record_type = tenv->Look(typ);
+  if (!record_type) {
+    errormsg.Error(pos, "undefined type %s", typ->Name().c_str());
+    return TY::VoidTy::Instance();
+  }
+  else {
+    if (record_type->ActualTy()->kind != TY::Ty::RECORD) {
+      errormsg.Error(pos, "%s is not a record", typ->Name().c_str());
+      return TY::VoidTy::Instance();
+    }
+    else {
+      TY::RecordTy *this_record_type = static_cast<TY::RecordTy*>(record_type);
+      TY::FieldList *fieldList = this_record_type->fields;
+      A::EFieldList *eFieldList = fields;
+      while (eFieldList) {
+        A::EField *eField = eFieldList->head;
+        TY::FieldList *fieldList_local = fieldList;
+        while (fieldList_local) {
+          if (eField->name->Name() == fieldList_local->head->name->Name())
+            break;
+          fieldList_local = fieldList_local->tail;
+        }
+        if (!fieldList_local) {
+          errormsg.Error(pos, "unknown field %s", eField->name->Name().c_str());
+          eFieldList = eFieldList->tail;
+          continue;
+        }
+        TY::Ty *fieldType = eField->exp->SemAnalyze(venv, tenv, labelcount);
+        if (!fieldType->IsSameType(fieldList_local->head->ty)) {
+          errormsg.Error(pos, "mismatched field type %s", eField->name->Name().c_str());
+          eFieldList = eFieldList->tail;
+          continue;
+        }
+        eFieldList = eFieldList->tail;
+      }
+    }
+  }
+  return record_type->ActualTy();
 }
 
 TY::Ty *SeqExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
   // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  A::ExpList *this_seq = seq;
+  TY::Ty *result;
+  while (this_seq) {
+    result = this_seq->head->SemAnalyze(venv, tenv, labelcount);
+    if (!this_seq->tail) {
+      return result->ActualTy();
+    }
+    this_seq = this_seq->tail;
+  }
+  return result->ActualTy();
 }
 
 TY::Ty *AssignExp::SemAnalyze(VEnvType venv, TEnvType tenv,
                               int labelcount) const {
   // TODO: Put your codes here (lab4).
+  TY::Ty *right_type = exp->SemAnalyze(venv, tenv, labelcount);
+  TY::Ty *left_type = var->SemAnalyze(venv, tenv, labelcount);
+  if (!left_type->IsSameType(right_type)) {
+    errormsg.Error(pos, "unmatched type");
+  }
+  if (var->kind == A::Var::SIMPLE) {
+    A::SimpleVar *simpleVar = static_cast<A::SimpleVar*>(var);
+    E::VarEntry *varEntry = static_cast<E::VarEntry*>(venv->Look(simpleVar->sym));
+    if (varEntry->readonly) {
+      errormsg.Error(pos, "loop variable cannot be assigned");
+    }
+  }
   return TY::VoidTy::Instance();
 }
 
 TY::Ty *IfExp::SemAnalyze(VEnvType venv, TEnvType tenv, int labelcount) const {
-  // TODO: Put your codes here (lab4).
-  return TY::VoidTy::Instance();
+  TY::Ty *testTy = test->SemAnalyze(venv, tenv, labelcount);
+  if (!testTy || testTy->IsSameType(TY::IntTy::Instance())) {
+    errormsg.Error(pos, "if integer required");
+  }
+  TY::Ty *thenTy = then->SemAnalyze(venv, tenv, labelcount);
+  if (!thenTy) {
+    errormsg.Error(pos, "if integer required"); // TODO
+  }
 }
 
 TY::Ty *WhileExp::SemAnalyze(VEnvType venv, TEnvType tenv,
