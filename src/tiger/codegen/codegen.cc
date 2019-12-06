e9477e69ca6d5acc8d5ebf5de96f42e286ba3921
Wang Tianxia <wang.tianxia@foxmail.com>
2019-12-06 08:04:15 +0000

    [lab5] Add function emit

--- src/tiger/codegen/codegen.cc
+++ src/tiger/codegen/codegen.cc
@@ -1,5 +1,21 @@
 #include "tiger/codegen/codegen.h"
 
+namespace {
+  AS::InstrList* iList = nullptr;
+  AS::InstrList* last = nullptr;
+
+  void emit(AS::Instr* inst) {
+    if (last) {
+      last->tail = new AS::InstrList(inst, nullptr);
+      last = last->tail;
+    }
+    else {
+      iList = new AS::InstrList(inst, nullptr);
+      last = iList;
+    }
+  }
+}
+
 namespace CG {
 
 AS::InstrList* Codegen(F::Frame* f, T::StmList* stmList) {
